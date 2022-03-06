9c88cac9171758f2776f5b30ffb6564f871ae383
Bulat <vyvavyva@gmail.com>
2022-03-06 19:36:39 +0400

    Added physics. Bugs: physics do not apply correctly, onbuttonclick works on button up

--- Assets/Scripts/Pause/Components/HasPause.cs
+++ Assets/Scripts/Pause/Components/HasPause.cs
@@ -1,15 +1,38 @@
+using System;
 using UnityEngine;
 
 namespace Pause.Components
 {
     public class HasPause : MonoBehaviour
     {
+        private Rigidbody _rigidbody;
+        private bool _isPhysical = false;
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            if (_rigidbody != null)
+            {
+                _isPhysical = true;
+            }
+        }
+
         public void Pause(bool isPaused)
         {
             var components = GetComponents<MonoBehaviour>();
             foreach (var behaviour in components)
             {
                 behaviour.enabled = !isPaused;
+            }
+
+            if (!_isPhysical) return;
+
+            if (isPaused)
+            {
+                _rigidbody.Sleep();
+            }
+            else
+            {
+                _rigidbody.WakeUp();
             }
         }
     }
